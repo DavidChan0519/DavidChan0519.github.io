b7527d5f9dc365cd86fbe37442a1d9c33e9d470f
wei.cheng <wei.cheng@bitmain.com>
2019-04-01 23:47:39 +0800

    Site updated: 2019-04-01 23:47:39

--- js/src/change-share-status.js
+++ js/src/change-share-status.js
@@ -1,20 +1,18 @@
 $(document).ready(function () { //初始化加载
     if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-                $("#fork-me-custom").hide();
+        $("#fork-me-custom").hide();
     }
 });
 
-//当菜单按钮隐藏时显示分享按钮，反之隐藏
+
 function showShare(){
     if ($(".site-nav-toggle").is(":hidden")) {
-            $("#fork-me-custom").show();
-                } else {
-                       $("#fork-me-custom").hide();
-                            }
-                            }
-                }
+        $("#fork-me-custom").show();
+    } else {
+        $("#fork-me-custom").hide();
     }
 }
+
 
 $(document).ready(function () {
   if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
